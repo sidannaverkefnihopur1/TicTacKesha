486c8a67e335a298dcb3fc6ac608c7da844f451d
Rafael <rafal12@ru.is>
2013-11-22 15:20:27 +0000

    TicAppToe.java added

---
+++ src/main/java/is/ru/tictactoe/TicAppToe.java
@@ -0,0 +1,32 @@
+package is.ru.TicTacToe;
+
+import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class TicAppToe {
+
+	private static final GameLogic gl = new GameLogic();
+	private static Board gb = new Board();
+
+	public static void main(String[] args) {
+
+		
+		staticFileLocation("/public");
+
+		//setPort(5678);
+
+		post( new Route("/move") {
+			@Override
+			public Object handle(Request request, Response response) {
+				String move = String.valueOf(request.queryParams("theMove"));
+				String player = String.valueOf(request.queryParams("thePlayer"));
+				int[] onField = gl.statementToInt(move);
+				gb.putOnField(onField, player.charAt(0));
+				if (gb.isWinner(player.charAt(0))) return "Winner";
+				else return "No winner...";
+			}
+		});
+	}
+}
